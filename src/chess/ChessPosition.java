b86e918d6556fc8d6cc0f44b19775c45cccbd519
Thyago Cantarelli <thyagocantarelli@gmail.com>
2024-01-17 01:17:21 -0300

    Moving Pieces

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -10,7 +10,7 @@ public class ChessPosition {
 	
 	public ChessPosition(char column, int row) {
 		if (row < 1 || row > 8 || column < 'a' || column > 'h') {
-			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8"); 
+			throw new ChessException("Erro na posição do tabuleiro. Os valores válidos são de a1 a h8"); 
 		}
 		this.column = column;
 		this.row = row;
